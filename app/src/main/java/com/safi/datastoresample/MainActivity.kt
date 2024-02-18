aa8904089beb4ba42bde796df7fef32f8d9fca63
Safi <ensahinsafi@gmai.com>
2024-02-19 01:07:50 +0600

    Minor changes.

--- app/src/main/java/com/safi/datastoresample/MainActivity.kt
+++ app/src/main/java/com/safi/datastoresample/MainActivity.kt
@@ -4,19 +4,32 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Telephony.Mms.Intents
+import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import com.safi.datastoresample.databinding.ActivityMainBinding
 import com.safi.datastoresample.utils.PrefDatastore
 import com.safi.datastoresample.utils.PrefKeys
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val prefDatastore by lazy { PrefDatastore(this) }
+
+    private val flow1 = flow {
+        delay(1000)
+        emit("Flow1")
+    }
+    private val flow2 = flow {
+        delay(3000)
+        emit("Flow2")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,21 +45,26 @@ class MainActivity : AppCompatActivity() {
 
     private fun observeUserInfo() {
 
-        CoroutineScope(Dispatchers.Main).launch {
+        lifecycleScope.launch {
+            flow1.collect {
+                Toast.makeText(this@MainActivity, it, Toast.LENGTH_LONG).show()
+            }
+
+            flow2.collect {
+                Toast.makeText(this@MainActivity, it, Toast.LENGTH_LONG).show()
+            }
+        }
+
+        lifecycleScope.launch {
             prefDatastore.observeString(PrefKeys.userName).collect {
                 binding.userNameValueTV.text = it
             }
+        }
+
+        lifecycleScope.launch {
             prefDatastore.observeString(PrefKeys.userEmail).collect {
                 binding.userEmailValueTV.text = it
             }
-        }
-
-        lifecycleScope.launch {
-
-        }
-
-        lifecycleScope.launch {
-
         }
 
         lifecycleScope.launch {
